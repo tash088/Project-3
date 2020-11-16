390f925b109db7266afc755b6fb13a56e3c10f22
Samantha Ashby <samanthaashby@Samanthas-MacBook-Pro.local>
2020-11-16 08:26:46 -0500

    Commit #3 on 11/16. Added comments and indentions to make code more readable.

--- server.R
+++ server.R
@@ -1,8 +1,15 @@
+#Author: Taylor Ashby
+#Date: November 2020
+#Purpose: Project #3 in ST558. This program is the server portion of the R Shiny 
+#application for Project #3.
+
+#Call required libraries
 library(ggplot2)
 library(caret)
 library(readxl)
 library(tidyverse)
 
+#Read in data and format as needed.
 creditData <- read_excel("creditCardData.xlsx",col_names=TRUE)
 creditData<-rename(creditData,default=`default payment next month`)
 creditData<-select(creditData,LIMIT_BAL,SEX,EDUCATION,MARRIAGE,AGE,PAY_0,default)
@@ -11,11 +18,11 @@ creditData$SEX<-as.factor(creditData$SEX)
 creditData$EDUCATION<-as.factor(creditData$EDUCATION)
 creditData$MARRIAGE<-as.factor(creditData$MARRIAGE)
 creditData$PAY_0<-creditData$PAY_0
-#to make processing quicker for now 
-creditData<-creditData[1:1000,]
 
+#Create Shiny server
 shinyServer(function(input, output, session) {
     
+    #Filter data based on user inputs
     getData <- reactive({
         #apply marriage status filter if applicable
         if(input$marRB=="Married"){creditData<-filter(creditData,MARRIAGE==1)}
@@ -28,16 +35,19 @@ shinyServer(function(input, output, session) {
         creditData
     })
     
-    #create plot
+    #Create plot for Data Exploration tab
     output$creditPlot <- renderPlot({
+      
         #get filtered data
         creditData <- getData()
         
-        #create plot
+        #Create plot
         g <- ggplot(creditData)
         
         if(input$gtype=="Boxplot"){
                 g <- g + geom_boxplot(aes_string(x="default",y=input$boxVar))
+            
+            #Customize y-axis scale based on variable selection
             if(input$boxVar=="LIMIT_BAL"){g + ylim(0,750000)}
                 else{g+ylim(0,80)}
                 
@@ -50,9 +60,10 @@ shinyServer(function(input, output, session) {
         
     })
     
+    #This code is a duplicate of the plot creation code above, but instead of displaying 
+    #the plot in the app, it will save it to a ".png" file if user clicks the download button.
     plotInput <- reactive({
-      
-      #create plot
+
       g <- ggplot(creditData)
       
       if(input$gtype=="Boxplot"){
@@ -60,15 +71,13 @@ shinyServer(function(input, output, session) {
         if(input$boxVar=="LIMIT_BAL"){g + ylim(0,750000)}
         else{g+ylim(0,80)}
         
-        
-        
       } else {
         g + geom_histogram(aes_string(x=input$histVar))
       }
       
-      
     })
     
+    #This saves the plot if the user clicks the download button (on Explore tab)
     output$downloadPlot <- downloadHandler(
       filename = function() { paste('creditPlot', '.png', sep='') },
       content = function(file) {
@@ -76,29 +85,29 @@ shinyServer(function(input, output, session) {
       }
     )
     
-    #create dynamic title
+    #Create dynamic title for Explore tab
     output$title<-renderUI({
-        paste0(input$gtype," of Credit Data")
+      h3(paste0(input$gtype," of Credit Data"))
     })
     
-    #Five number summary
+    #Allow user to click anywhere on plot and have coordinates displayed
+    output$click_info <- renderText({
+      paste0("x=", format(input$plot_click$x,digits=2), "\ny=", format(input$plot_click$y,digits=2))
+    })
+    
+    #Five number summary for display on Explore tab
     output$numSummary <- renderPrint({
         summary(select(creditData,LIMIT_BAL,EDUCATION,SEX,MARRIAGE,AGE,PAY_0))
  
     })
     
-    output$info<-renderText({
-        "This app was built using R Shiny. It allows the user to explore data on 
-        credit card defaults in Taiwan. The data comes from the UCI learning repository, 
-        <click here> for more detail"
-    })
-    
+    #Add note to cluster page noting that the data has been subsetted.
     output$cluster<-renderText({
         "This is a dendrogram showing the hierarchical clustering of
           the first 500 observations in our data set"
     })
     
-
+    #Create dendrogram to display clustering results
     output$dend<-renderPlot({
         #to make processing quicker for now 
         creditData<-creditData[1:500,]
@@ -108,23 +117,19 @@ shinyServer(function(input, output, session) {
         plot(hierClust, xlab = paste0("Dendrogram Showing Clustering of ",input$var1," and ", input$var2))
     })
     
-    output$model<-renderText({
-        paste0("modeling goes here",input$preds)
+    #Create text including MathJax equation on Modeling tab
+    output$mJ <- renderUI({
+            if(input$mType=="Logistic Regression"){
+             withMathJax(
+          helpText('When Y is categorical, as in this case, for logistic regression 
+         we use the logit of Y as the response in our regression equation instead of just Y: 
+                 $$log(\\frac{P}{1-P}) = \\beta_0+\\beta_1X_1+\\beta_2X_2+...++\\beta_kX_k$$')
+        )
+            }
+    
     })
     
-  
-        output$mJ <- renderUI({
-                if(input$mType=="Logistic Regression"){
-                 withMathJax(
-              helpText('When Y is categorical, as in this case, for logistic regression 
-             we use the logit of Y as the response in our regression equation instead of just Y: 
-                     $$log(\\frac{P}{1-P}) = \\beta_0+\\beta_1X_1+\\beta_2X_2+...++\\beta_kX_k$$')
-            )
-                }
-        
-        })
-    
-    
+    #Fit Logistic Regression or Tree Model and output results
     output$mResults<-renderPrint({
     if(input$mType=="Logistic Regression"){
      if(input$preds=="All"){
@@ -161,6 +166,8 @@ shinyServer(function(input, output, session) {
     }
     })
     
+    #Allow user to predict the probability of default based on payment status in prior month
+    #i.e., PAY_0 variable
     output$pResults<-renderPrint({
       myexp <- paste0("default", "~", "PAY_0")
       if(input$mType=="Logistic Regression"){
@@ -172,6 +179,7 @@ shinyServer(function(input, output, session) {
           predPay<-predict(fitLogitPred,newdata= data.frame(PAY_0=c(myPred)),type="prob")
           paste0("Probability of default is: ",predPay[1,2])
       }
+      
       else{
         fitTreePred<-train(as.formula(myexp), data=creditData, method="rpart",
                            preProcess=c("center","scale"),
@@ -185,6 +193,7 @@ shinyServer(function(input, output, session) {
        
     })
     
+    #Display raw data on Data tab, including options for user to subset
     output$dataTable <- renderTable({
         #apply marriage status filter if applicable
         if(input$marRBDat=="Married"){creditData<-filter(creditData,MARRIAGE==1)}
@@ -198,12 +207,6 @@ shinyServer(function(input, output, session) {
         view(creditData)
         
     })
-    
-    output$click_info <- renderText({
-        paste0("x=", format(input$plot_click$x,digits=2), "\ny=", format(input$plot_click$y,digits=2))
-    })
-    
-    
 
     }
 )
--- ui.R
+++ ui.R
@@ -1,8 +1,15 @@
+#Author: Taylor Ashby
+#Date: November 2020
+#Purpose: Project #3 in ST558. This program is the UI portion of the R Shiny 
+#application for Project #3.
+
+#Call required libraries
 library(ggplot2) 
 library(caret) 
 library(readxl) 
 library(tidyverse)
 
+#Read in data and format as needed.
 creditData <- read_excel("creditCardData.xlsx",col_names=TRUE)
 creditData<-rename(creditData,default=`default payment next month`)
 creditData<-select(creditData,LIMIT_BAL,SEX,EDUCATION,MARRIAGE,AGE,PAY_0,default)
@@ -13,9 +20,7 @@ creditData$MARRIAGE<-as.factor(creditData$MARRIAGE)
 creditData$PAY_0<-creditData$PAY_0
 creditDataPreds<-select(creditData,-default)
 
-#to make processing quicker for now 
-creditData<-creditData[1:1000,]
-
+#Create Shiny UI
 shinyUI(fluidPage(
     
     withMathJax(),
@@ -26,148 +31,169 @@ shinyUI(fluidPage(
     ),
     
 
-    # Sidebar with options for the data set
+    # Sidebar with options including subsetting, plot type selection, etc.
     sidebarLayout(
         sidebarPanel(
+            
             #sidebar settings for "Explore" tab
             conditionalPanel(condition="input.tabselected==2",
             
-            radioButtons("marRB","Subset by Married Status?",
-                         selected = "Do Not Subset",
-                         choices=list("Married","Single","Do Not Subset")),
+                radioButtons("marRB","Subset by Married Status?",
+                            selected = "Do Not Subset",
+                            choices=list("Married","Single","Do Not Subset")),
             
-            radioButtons("edRB","Subset by Education?",
-                         selected = "Do Not Subset",
-                         choices=list("Grad School","University","Highschool","Other","Do Not Subset")),
+                radioButtons("edRB","Subset by Education?",
+                            selected = "Do Not Subset",
+                            choices=list("Grad School","University","Highschool","Other","Do Not Subset")),
             
-            selectizeInput("gtype", "Select graph type:", selected = "Histogram", 
-                           choices = c("Histogram","Boxplot")),
+                selectizeInput("gtype", "Select graph type:", selected = "Histogram", 
+                                choices = c("Histogram","Boxplot")),
             
-            conditionalPanel(condition="input.gtype=='Histogram'",
-                             selectizeInput("histVar", "Select variable for histogram:", 
+                #Variable selection if plot type is histogram
+                conditionalPanel(condition="input.gtype=='Histogram'",
+                                 selectizeInput("histVar", "Select variable for histogram:", 
                                             selected = "LIMIT_BAL", 
                                             choices = c("LIMIT_BAL","AGE")),
-            ),
-            
-            conditionalPanel(condition="input.gtype=='Boxplot'",
-                             selectizeInput("boxVar", "Select y variable for boxplot:", 
+                ),
+                
+                #Variable selection if plot type is Boxplot
+                conditionalPanel(condition="input.gtype=='Boxplot'",
+                                selectizeInput("boxVar", "Select y variable for boxplot:", 
                                             selected = "LIMIT_BAL", 
                                             choices = c("LIMIT_BAL","AGE")),
+                ),
+            
+                #Allow user to download and save the plot image.
+                downloadButton('downloadPlot', 'Download Plot')
+            
             ),
             
-            
-            downloadButton('downloadPlot', 'Download Plot')
-            
-            ),
-            
-            #sidebar settings for "Clustering" tab
+            #Sidebar settings for "Clustering" tab
             conditionalPanel(condition="input.tabselected==3",
                              
-                             selectizeInput("var1", "Select Variable 1", selected = names(creditData)[[5]], 
-                                            choices = names(creditData)),
-                             selectizeInput("var2", "Select Variable 2", selected = names(creditData)[[1]], 
-                                            choices = names(creditData)),
-                             
-                             selectizeInput("clustMeth", "Select agglomeration method", 
-                                            selected = "complete", 
-                                            choices = c("complete","single","average","centroid")),
+                 #Select first variable for clustering
+                 selectizeInput("var1", "Select Variable 1", selected = names(creditData)[[5]], 
+                                choices = names(creditData)),
+                 
+                 #Select second variable for clustering
+                 selectizeInput("var2", "Select Variable 2", selected = names(creditData)[[1]], 
+                                choices = names(creditData)),
+                 
+                 #Allow user to customize agglomeration method for clustering
+                 selectizeInput("clustMeth", "Select agglomeration method", 
+                                selected = "complete", 
+                                choices = c("complete","single","average","centroid")),
             ),
+            
+            #Sidebar settings for "Modeling" tab
             conditionalPanel(condition="input.tabselected==4",
-                             selectizeInput("mType","Select Model Type",
-                                            selected="Logistic Regression",
-                                            choices=c("Logistic Regression","Decision Tree")),
-                                
-                            selectizeInput("preds","Select Predictor Variable(s)",
-                                                selected="All",
-                                                choices=c("All",names(creditDataPreds))),
+                
+                #Select model type             
+                selectizeInput("mType","Select Model Type",
+                                selected="Logistic Regression",
+                                choices=c("Logistic Regression","Decision Tree")),
+                
+                #Select predictor variables    
+                selectizeInput("preds","Select Predictor Variable(s)",
+                                    selected="All",
+                                    choices=c("All",names(creditDataPreds))),
+                
+                #User can customize # of folds for cross-validation
+                numericInput("cvFolds","Choose # of Folds for Cross-Validation (Between 2 and 10)",
+                             value=5,min=2,max=10,step=1),
+                
+                "The results indicate that PAY_0 (# of months behind in prior month, where 0 indicates current) 
+                has the most predictive power. Use the dropdown below to predict the probability of default based
+                on the PAY_0 value.",
+                        
+                #User can select a value for the PAY_0 for prediction
+                selectizeInput("payPred","Select a value for PAY_0 to see the resulting probability of default:",
+                               selected=c(0),
+                               choices=c(-2,-1,0,1:9)),
                             
-                            numericInput("cvFolds","Choose # of Folds for Cross-Validation (Between 2 and 10)",
-                                         value=5,min=2,max=10,step=1),
-                            
-                            "The results indicate that PAY_0 (# of months behind in prior month, where 0 indicates current) 
-                            has the most predictive power. Use the dropdown below to predict the probability of default based
-                            on the PAY_0 value.",
-                            
-                            
-                            selectizeInput("payPred","Select a value for PAY_0 to see the resulting probability of default:",
-                                           selected=c(0),
-                                           choices=c(-2,-1,0,1:9)),
-                            
-                             ),
-            #sidebar settings for "Data" tab
+            ),
+            
+            #Sidebar settings for "Data" tab
             conditionalPanel(condition="input.tabselected==5",
-                             
-                             radioButtons("marRBDat","Subset by Married Status?",
-                                          selected = ("Do Not Subset"),
-                                          choices=list("Married","Single","Do Not Subset")),
-                             
-                             radioButtons("edRBDat","Subset by Education?",
-                                          selected = ("Do Not Subset"),
-                                          choices=list("Grad School","University","Highschool","Other","Do Not Subset")),
-                             actionButton("save","Save CSV")
-                             
-         
-                             
+                
+                #Options to subset by marriage and/or education level                          
+                 radioButtons("marRBDat","Subset by Married Status?",
+                              selected = ("Do Not Subset"),
+                              choices=list("Married","Single","Do Not Subset")),
+                 
+                 radioButtons("edRBDat","Subset by Education?",
+                              selected = ("Do Not Subset"),
+                              choices=list("Grad School","University","Highschool","Other","Do Not Subset")),
+                
+                #Button to allow user to save the data as a CSV file
+                actionButton("save","Save CSV")
             )
             
             
-            ),
+        ),
     
     mainPanel(
         
-        
-        # Tabset w/ Information, Data Exploration, Clustering, Modeling, Data.
+        # Create tabs for Information, Data Exploration, Clustering, Modeling, Data.
         tabsetPanel(type = "tabs",
-                    tabPanel("Info", value=1,
-                             h3("General Information"),
-                             "This app, designed with R Shiny, can be used to conduct exploratory data
-                             analysis and modeling on Taiwanese credit card default data.  
-                              More details on the data set are available on the ",
-                             a(href="https://archive.ics.uci.edu/ml/datasets/default+of+credit+card+clients",
-                               target="_blank","UCI Machine Learning website"),
-                             ".",
-                             br(),
-                             br(),
-                             "For simplicity, the data has been subsetted to excluded 
-                             the time-series variables. The modeling page will 
-                             allow the user to run",
-                             em("Logistic Regression"),
-                             "and/or",
-                             em("Decision Trees"),
-                             "models to predict whether a borrower will default. On the data tab, 
-                             you can view the raw data and save it out to a CSV
-                             file if desired. Simply click on the tabs at the top of the page to 
-                             navigate between them.",
-                             br(),
-                             br(),
-                             "Note that many of the tabs have options to filter the data and/or customize
-                             the output, so keep an eye out for those options in the sidebar panel."
-                             ),
-                    tabPanel("Explore", value=2,
-                             uiOutput("title"),
-                             "Click a point on graph to get x/y values",
-                             verbatimTextOutput("click_info"),
-                             plotOutput("creditPlot",click = "plot_click"),
-                             verbatimTextOutput("numSummary")
-                             ),
-                    
-                    tabPanel("Clustering", value=3,
-                             textOutput("cluster"),
-                             plotOutput("dend")
-                    ),
-                    
-                    tabPanel("Modeling", value=4,
-                             uiOutput("mJ"),
-                             textOutput("model"),
-                            verbatimTextOutput("mResults"),
-                            verbatimTextOutput("pResults")
-                             ),
-                    
-                    tabPanel("Data", value=5,
-                             textOutput("data"),
-                             tableOutput("dataTable")
-                             ),
-                    id="tabselected"
+            
+            #Output for Information tab
+            tabPanel("Info", value=1,
+                     h3("General Information"),
+                     "This app, designed with R Shiny, can be used to conduct exploratory data
+                     analysis and modeling on Taiwanese credit card default data.  
+                      More details on the data set are available on the ",
+                     a(href="https://archive.ics.uci.edu/ml/datasets/default+of+credit+card+clients",
+                       target="_blank","UCI Machine Learning website"),
+                     ".",
+                     br(),
+                     br(),
+                     "For simplicity, the data has been subsetted to excluded 
+                     the time-series variables. The modeling page will 
+                     allow the user to run",
+                     em("Logistic Regression"),
+                     "and/or",
+                     em("Decision Trees"),
+                     "models to predict whether a borrower will default. On the data tab, 
+                     you can view the raw data and save it out to a CSV
+                     file if desired. Simply click on the tabs at the top of the page to 
+                     navigate between them.",
+                     br(),
+                     br(),
+                     "Note that many of the tabs have options to filter the data and/or customize
+                     the output, so keep an eye out for those options in the sidebar panel."
+                     ),
+            
+            #Output for Data Exploration tab
+            tabPanel("Explore", value=2,
+                     uiOutput("title"),
+                     "Click a point on graph to get x/y values",
+                     verbatimTextOutput("click_info"),
+                     plotOutput("creditPlot",click = "plot_click"),
+                     verbatimTextOutput("numSummary")
+                     ),
+            
+            #Output for Clustering tab
+            tabPanel("Clustering", value=3,
+                     textOutput("cluster"),
+                     plotOutput("dend")
+            ),
+            
+            #Output for Modeling tab
+            tabPanel("Modeling", value=4,
+                     uiOutput("mJ"),
+                     textOutput("model"),
+                    verbatimTextOutput("mResults"),
+                    verbatimTextOutput("pResults")
+                     ),
+            
+            #Output for Data tab
+            tabPanel("Data", value=5,
+                     textOutput("data"),
+                     tableOutput("dataTable")
+                     ),
+            
+            id="tabselected"
         )
     )
     )
